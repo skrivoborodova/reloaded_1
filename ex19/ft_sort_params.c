5a4ad51ffc09407881e178cdf901e0914df657b1
Oakenshield Earlene <oearlene@at-j5.21-school.ru>
2019-09-06 02:42:56 +0300

    ex19

--- ex19/ft_sort_params.c
+++ ex19/ft_sort_params.c
@@ -6,16 +6,11 @@
 /*   By: oearlene <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/05 04:10:26 by oearlene          #+#    #+#             */
-/*   Updated: 2019/09/06 02:27:29 by oearlene         ###   ########.fr       */
+/*   Updated: 2019/09/06 02:41:10 by oearlene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
+void	ft_putchar(char c);
 
 void	ft_putstr(char *str)
 {
@@ -51,25 +46,24 @@ int		ft_strcmp(char *s1, char *s2)
 int		main(int ac, char **av)
 {
 	int res;
-	
+
 	ac = 2;
-		while (av[ac] && av[ac - 1])
+	while (av[ac] && av[ac - 1])
+	{
+		res = ft_strcmp(av[ac - 1], av[ac]);
+		if (res > 0)
 		{
-			res = ft_strcmp(av[ac - 1], av[ac]);
-			if (res > 0)
-			{
-				ft_swap(&av[ac - 1], &av[ac]);
-				ac = 1;
-			}
-			ac++;
+			ft_swap(&av[ac - 1], &av[ac]);
+			ac = 1;
 		}
-		ac = 1;
-		while (av[ac])
-		{
-			ft_putstr(av[ac]);
-			ft_putchar('\n');
-			ac++;
-		}
-	
-		return (0);
+		ac++;
+	}
+	ac = 1;
+	while (av[ac])
+	{
+		ft_putstr(av[ac]);
+		ft_putchar('\n');
+		ac++;
+	}
+	return (0);
 }
