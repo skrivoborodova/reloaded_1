a6a9a13fc1d228b29c06a2f93408e8e89cbde1d2
Oakenshield Earlene <oearlene@at-j5.21-school.ru>
2019-09-06 03:46:25 +0300

    ex20

---
+++ ex20/ft_strdup.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oearlene <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/06 02:59:10 by oearlene          #+#    #+#             */
+/*   Updated: 2019/09/06 03:46:06 by oearlene         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	char	*str;
+	char	*s;
+	int		i;
+
+	i = 0;
+	while (src[i])
+		i++;
+	if (!(str = (char *)malloc(sizeof(*str) * (i + 1))))
+		return (NULL);
+	else
+	{
+		s = str;
+		while (*src)
+			*s++ = *src++;
+		*s = '\0';
+		return (str);
+	}
+}
