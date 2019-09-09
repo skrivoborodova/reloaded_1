3af195cf73da3482a8eaa6b3bf03b146f11958c0
Oakenshield Earlene <oearlene@at-i6.21-school.ru>
2019-09-09 23:54:40 +0300

    working prog less than 2 sec

--- ex14/ft_sqrt.c
+++ ex14/ft_sqrt.c
@@ -6,23 +6,52 @@
 /*   By: oearlene <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/05 03:34:23 by oearlene          #+#    #+#             */
-/*   Updated: 2019/09/08 23:10:07 by oearlene         ###   ########.fr       */
+/*   Updated: 2019/09/09 23:53:57 by oearlene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <time.h>
+#include <stdio.h>
 
 int	ft_sqrt(int nb)
 {
 	int i;
+	int res;
 
-	i = 0;
-	if (nb == 0)
+	res = 0;
+	i = 1;
+	if (nb <= 0)
 		return (0);
-	while (i * i < nb)
+	while (i <= nb / 2)
 	{
+		res = i * i;
+		if (res == nb)
+			return (i);
 		i++;
 	}
-	if ((nb % i) == 0)
-		return (i);
-	else
-		return (0);
+}
+
+int	main(void)
+{
+	clock_t tic = clock();
+
+	printf("Sq -1523 donne %d\n", ft_sqrt(-101));
+	printf("0 donne %d\n", ft_sqrt(0));
+	printf("214748364 donne %d\n", ft_sqrt(214748364));
+	printf("268435456 donne %d\n", ft_sqrt(268435456));
+	printf("134167199 donne %d\n", ft_sqrt(134167199));
+	printf("90935296 donne %d\n", ft_sqrt(90935296));
+	printf("92260 donne %d\n", ft_sqrt(92260));
+	printf("716632900 donne %d\n", ft_sqrt(716632900));
+	printf("62904750  donne %d\n", ft_sqrt(62904750));
+	printf("1545040249 donne %d\n", ft_sqrt(1545040249));
+	printf("70765342 = %d\n", ft_sqrt(70765342));
+	printf("68807025 = %d\n", ft_sqrt(68807025));
+	printf("46629167 = %d\n", ft_sqrt(46629167));
+
+   clock_t toc = clock();
+
+	printf("Elapsed: %f seconds\n", (double)(toc - tic) / CLOCKS_PER_SEC);
+
+	return (0);
 }
